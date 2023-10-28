4fab36a54d3c12ebfd736fbd0cc8907d9baf269a
Samudhi1220 <samudhirandula@icloud.com>
2023-10-28 18:52:20 +0530

    updated

--- assignment/JS/InMemoryPOS/controller/PurchaseOrderController.js
+++ assignment/JS/InMemoryPOS/controller/PurchaseOrderController.js
@@ -1,4 +1,4 @@
-let orderID;
+let orderIDs;
 let orderedCustomerId;
 let orderDate;
 let orderedItemId;
@@ -9,14 +9,23 @@ let discount;
 let order;
 let orderDetail;
 let finalBalance;
-let typeCash;
+let inputCash ;
 let subTotal;
-let balance;
+let getbalance;
 $('#customer').prop('disabled', true);
 $('#address').prop('disabled', true);
 $('#itemNam').prop('disabled', true);
 $('#qtyOnHand').prop('disabled', true);
 $('#price').prop('disabled', true);
+$('#itemIdSpan').css('display', 'none');
+$('#inputItemCodes').css("border", "1px solid #ced4da");
+$('#cusIdSpan').css('display', 'none');
+$('#inputCustomerId').css('border', '1px solid #ced4da');
+$('#dateSpan').css('display', 'none');
+$('#date').css('border', '1px solid #ced4da');
+$('#orderQtySpan').css('display', 'none');
+$('#orderQTY').css('border', '1px solid #ced4da');
+$('#cashSpan').css('display','none');
 function setCustomerId() {
     $('#inputCustomerId').empty();
     $('#inputCustomerId').append('<option selected>Choose...</option>');
@@ -84,3 +93,232 @@ $('#date').on('change', function () {
 
 })
 
+$('#cash').on('keydown keyup',function () {
+
+    getbalance =parseInt($('#cash').val())- parseInt($('#totalSpan').text());
+      $('#balance').val(getbalance);
+
+      if ( $('#balance').val()== "NaN"){
+          $('#balance').val("");
+
+      }
+      inputCash= $('#cash').val();
+      finalBalance = $('#balance').val();
+    if($('#discount').val() != ""){
+
+            $('#balance').val(inputCash-subTotal);
+    }
+
+insufficient();
+})
+
+$('#discount').on('keyup',function () {
+    discount= parseInt($('#discount').val())/100* parseInt($('#totalSpan').text());
+
+    subTotal= $('#totalSpan').text()-discount;
+
+    $('#subTotalSpan').text(subTotal);
+
+    if ( $('#subTotalSpan').text() == "NaN"){
+        $('#subTotalSpan').text($('#totalSpan').text());
+    }
+    $('#balance').val(inputCash-subTotal);
+    if($('#balance').val() == "NaN"){
+        $('#balance').val(getbalance);
+    }
+    finalBalance = $('#balance').val();
+    insufficient();
+})
+
+$('#totalSpan').text(100);
+$('#subTotalSpan').text(100);
+
+function orderAddToCart() {
+    orderIDs = $('#orderId').val();
+    orderQty = $('#orderQTY').val();
+    price = $('#price').val();
+
+    order = Object.assign({}, purchaseOrder);
+    orderDetail = Object.assign({}, orderDetails);
+
+    order.oid = orderIDs;
+    order.date = orderDate;
+    order.customerID = orderedCustomerId;
+
+    orderDetail.oid = orderIDs;
+    orderDetail.qty = orderQty;
+    orderDetail.unitPrice = price;
+    orderDetail.code = orderedItemId;
+
+    if (searchExistCartItem(orderedItemId)) {
+        getDataByItemID(order.orderDetails, orderedItemId).qty = parseInt(getDataByItemID(order.orderDetails, orderedItemId).qty) + parseInt(orderQty);
+    } else {
+        order.orderDetails.push(orderDetail);
+    }
+    let total = 0;
+    $('#tblOrderDetails').empty();
+    for (let orderElement of order.orderDetails) {
+        var row = `<tr><td>${orderElement.code}</td><td>${getDataById(itemDB, orderElement.code).name}</td><td>${order.date}</td><td>${orderElement.unitPrice}</td><td>${orderElement.qty}</td><td>${parseInt(orderElement.unitPrice) * parseInt(orderElement.qty)}</td></tr>`;
+        $('#tblOrderDetails').append(row)
+
+        total += parseInt(orderElement.unitPrice) * parseInt(orderElement.qty);
+
+        $('#totalSpan').text(total);
+        $('#subTotalSpan').text(total);
+        $('#orderQTY').val('');
+        setBtnOrder();
+    }
+}
+
+function searchExistCartItem(id) {
+    return order.orderDetails.find(function (orderDetail) {
+        return orderDetail.code == id;
+    });
+}
+
+
+function getDataByItemID(arr, id) {
+    let index = -1;
+
+    for (let Obj of arr) {
+        if (Obj.code == id) {
+            index = arr.indexOf(Obj);
+            return arr[index];
+        }
+    }
+    return null;
+}
+
+function getDataById(arr, id) {
+    let index = -1;
+
+    for (let Obj of arr) {
+        if (Obj.code == id) {
+            index = arr.indexOf(Obj);
+            return arr[index];
+        }
+    }
+    return null;
+}
+
+$('#btnAddItem').click(function () {
+    if (checkAllOrderRegex()) {
+        if ($('#inputItemCodes').val() == 'Choose...') {
+            $('#itemIdSpan').css('display', 'block');
+            $('#inputItemCodes').css('border', '1px solid red');
+        }
+        if ($('#inputCustomerId').val() == 'Choose...') {
+            $('#cusIdSpan').css('display', 'block');
+            $('#inputCustomerId').css('border', '1px solid red');
+        }
+        if ($('#date').val() == '') {
+            $('#dateSpan').css('display', 'block');
+            $('#date').css('border', '1px solid red');
+        }
+        if ($('#inputItemCodes').val() != 'Choose...' && $('#inputCustomerId').val() != 'Choose...' && $('#date').val() != '') {
+            orderAddToCart();
+        }
+    } else {
+        alert('error');
+    }
+});
+
+
+$('#btnAddPlaceOrder').click(function () {
+    if (checkAllPlaceOrderRegex()) {
+        if ($('#inputItemCodes').val() == 'Choose...') {
+            $('#itemIdSpan').css('display', 'block');
+            $('#inputItemCodes').css('border', '1px solid red');
+        }
+        if ($('#inputCustomerId').val() == 'Choose...') {
+            $('#cusIdSpan').css('display', 'block');
+            $('#inputCustomerId').css('border', '1px solid red');
+        }
+        if ($('#date').val() == '') {
+            $('#dateSpan').css('display', 'block');
+            $('#date').css('border', '1px solid red');
+        }
+        if ($('#inputItemCodes').val() != 'Choose...' && $('#inputCustomerId').val() != 'Choose...' && $('#date').val() != '') {
+            placeOrder();
+        }
+    } else {
+        alert('error');
+    }
+});
+ function insufficient() {
+     if (finalBalance.startsWith("-")){
+         $('#cashSpan').css('display','block');
+     }else{
+         $('#cashSpan').css('display','none');
+     }
+ }
+
+function searchExistOrder(id) {
+    return orderDB.find(function (order) {
+        return order.oid == id;
+    });
+}
+
+function placeOrder() {
+
+    cash = $('#cash').val();
+    discount = $('#discount').val();
+
+
+    if (searchExistOrder(orderIDs.trim())) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'This Order Already Exist.'
+        });
+    } else {
+        orderDB.push(order);
+        purchaseOrder.orderDetails = [];
+
+        let index = -1;
+
+        for (let orderObj of orderDB) {
+            if (orderObj.oid == $('#orderId').val()) {
+                index = orderDB.indexOf(orderObj);
+            }
+        }
+
+        for (let i = 0; i < orderDB[index].orderDetails.length; i++) {
+            let itemCode = orderDB[index].orderDetails[i].code;
+
+            for (let itemDBElement of itemDB) {
+                if (itemDBElement.id == itemCode) {
+                    itemDBElement.qty = itemDBElement.qty - orderDB[index].orderDetails[i].qty;
+                }
+            }
+        }
+
+        loadAllItemData();
+        $('#orderId').val('');
+        $('#date').val('');
+        $('#customer').val('');
+        $('#address').val('');
+        $('#itemNam').val('');
+        $('#price').val('');
+        $('#qtyOnHand').val('');
+        $('#cash').val('');
+        $('#discount').val('');
+        $('#balance').val('');
+        $('#inputItemCodes').val('Choose...');
+        $('#inputCustomerId').val('Choose...');
+        $('#btnAddPlaceOrder').prop('disabled', true);
+        $('#totalSpan').text('00.00');
+        $('#subTotalSpan').text('00.00');
+        $('#tblOrderDetails').empty();
+
+
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Customer has been saved',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    }
+
+}
--- assignment/JS/InMemoryPOS/controller/validation/PurchaseOrderValidation.js
+++ assignment/JS/InMemoryPOS/controller/validation/PurchaseOrderValidation.js
@@ -1,11 +1,29 @@
 const ORDER_ID_REGEX = /^(OR00-)[0-9]{3}$/;
 const QTY_REGEX = /[0-9]{1,9}/;
+const PRICE_REGEX = /^[0-9]{2,}([.][0-9]{2})?$/;
+const DISCOUNT_REGEX = /^[0-9]{1,}([.][0-9]{2})?$/;
 
 let o_vArray = new Array();
 o_vArray.push({field: $("#orderId"), regEx: ORDER_ID_REGEX});
 o_vArray.push({field: $("#orderQTY"), regEx: QTY_REGEX});
 
+let place_order_vArray = new Array();
+place_order_vArray.push({field: $("#cash"), regEx: PRICE_REGEX});
+place_order_vArray.push({field: $("#discount"), regEx: DISCOUNT_REGEX});
+place_order_vArray.push({field: $("#orderId"), regEx: ORDER_ID_REGEX});
+
+$("#btnAddPlaceOrder").prop("disabled", true);
+$("#balance").prop("disabled", true);
 function checkOrderValidation(object) {
+    if (object.regEx.test(object.field.val())) {
+        setOrderTextBorder(true, object);
+        return true;
+    }
+    setOrderTextBorder(false, object);
+    return false;
+}
+
+function checkPlaceOrderValidation(object) {
     if (object.regEx.test(object.field.val())) {
         setOrderTextBorder(true, object);
         return true;
@@ -38,8 +56,18 @@ function checkAllOrderRegex() {
     }
     return true;
 }
+function checkAllPlaceOrderRegex() {
+    for (let i = 0; i < place_order_vArray.length; i++) {
+        if (!checkPlaceOrderValidation(place_order_vArray[i])) {
+            return false;
+        }
+    }
+    return true;
+}
 focusNextOrderTextField();
 setBtnOrder();
+focusNextPlaceOrderTextField();
+setBtnPlaceOrder();
 function focusNextOrderTextField() {
     $('#orderQTY,#orderId').on('keyup', function (e) {
         let indexNo = o_vArray.indexOf(o_vArray.find((c) => c.field.attr("id") == e.target.id));
@@ -72,9 +100,64 @@ function checkOrderedQTY() {
         if (parseInt($('#orderQTY').val()) <= parseInt($('#qtyOnHand').val()) && parseInt($('#orderQTY').val()) != 0) {
             $('#orderQtySpan').css('display', 'none');
             $('#orderQTY').css('border', '1px solid #ced4da');
+           }else {
+        $('#orderQtySpan').text('Please Enter a Amount lower than: ' + parseInt($('#qtyOnHand').val()));
 
+        $('#orderQtySpan').css('display', 'block');
+        $('#orderQTY').css('border', '1px solid red');
+
+    }
+    } else {
+    $('#orderQtySpan').text('Please select item');
+    $('#orderQtySpan').css('display', 'block');
+    $('#orderQTY').css('border', '1px solid red');
 }
+
+if ($('#orderQTY').val() == '') {
+    $('#orderQtySpan').css('display', 'none');
+    $('#orderQTY').css('border', '1px solid #ced4da');
+}
+}
+$('#orderQTY').on('keyup', function () {
+    checkOrderedQTY();
+});
+
+function focusNextPlaceOrderTextField() {
+    $('#cash,#discount,#orderId').on('keyup', function (e) {
+        let indexNo = place_order_vArray.indexOf(place_order_vArray.find((c) => c.field.attr("id") == e.target.id));
+
+        if (e.key == "Tab") {
+            e.preventDefault();
+        }
+        let checker = checkOrderValidation(place_order_vArray[indexNo]);
+
+        if (e.key == "Enter") {
+
+            if ($('#cash').val() != "" && checker) {
+                $('#discount').focus();
+            }
+
+            if ($('#discount').val() != "" && checker) {
+                $('#btnAddPlaceOrder').click();
+            }
+
+        }
+        setBtnPlaceOrder();
+    });
+}
+
+function setBtnPlaceOrder() {
+
+    if (checkAllPlaceOrderRegex()) {
+        if (finalBalance.startsWith('-')) {
+            // $("#cashSpan").css("display", 'block');
+            $("#btnAddPlaceOrder").prop("disabled", true);
+        } else {
+            // $("#cashSpan").css("display", 'none');
+            $("#btnAddPlaceOrder").prop("disabled", false);
+        }
+
+    } else {
+        $("#btnAddPlaceOrder").prop("disabled", true);
     }
 }
-
-
